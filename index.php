3952c175486993878e05382c98fdce7eadaa84b8
Winter-0101 <quangthien1310@gmail.com>
2023-10-24 23:30:51 +0700

    Reach us

--- index.php
+++ index.php
@@ -365,6 +365,51 @@
         </div>
     </div>
 
+    <!-- Tìm chúng tôi -->
+    <h2 class="mt-5 pt-4 mb-4 text-center fw-bold h-font">TÌM CHÚNG TÔI</h2>
+
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-8 col-md-8 p-4 mb-lg-0 mb-3 bg-white rounded">
+                <iframe class="w-100 rounded" height="320px" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d7449.419251202785!2d105.84588999999998!3d21.0042738!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3135ac768ffe1abd%3A0x22b136bcf1c08e2a!2zQsOhY2ggS2hvYSwgSGFpIELDoCBUcsawbmcsIEjDoCBO4buZaQ!5e0!3m2!1svi!2s!4v1698153719874!5m2!1svi!2s" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+            </div>
+            <div class="col-lg-4 col-md-4">
+                <div class="bg-white p-4 rounded mb-4">
+                    <h5>Liên hệ</h5>
+                    <a href="tel: +84123456789" class="d-inline-block mb-2 text-decoration-none text-dark">
+                      <i class="bi bi-telephone-fill"></i> +84123456789
+                    </a>
+                    <br>
+                    <a href="tel: +84123456789" class="d-inline-block text-decoration-none text-dark">
+                      <i class="bi bi-telephone-fill"></i> +84123456789
+                    </a>
+                </div>
+                <div class="bg-white p-4 rounded mb-4">
+                    <h5>Theo dõi</h5>
+                    <a href="#" class="d-inline-block mb-3">
+                      <span class="badge bg-light text-dark fs-6 p-2">
+                      <i class="bi bi-twitter-x"></i> X
+                      </span>
+                    </a>
+                    <br>
+                    <a href="#" class="d-inline-block mb-3">
+                      <span class="badge bg-light text-dark fs-6 p-2">
+                      <i class="bi bi-facebook"></i> Facebook
+                      </span>
+                    </a>
+                    <br>
+                    <a href="#" class="d-inline-block mb-3">
+                      <span class="badge bg-light text-dark fs-6 p-2">
+                      <i class="bi bi-instagram"></i> Instagram
+                      </span>
+                    </a>
+                </div>
+            </div>
+        </div>
+    </div>
+    <br><br><br>
+    <br><br><br>
+
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/swiper@10/swiper-bundle.min.js"></script>
 
