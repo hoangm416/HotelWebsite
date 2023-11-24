c9f321ff9e3218735fd5a1615b900bcace03b3ca
messiuuuuuu <145339425+messiuuuuuu@users.noreply.github.com>
2023-11-24 23:15:49 +0700

    Add files via upload

--- admin/dashboard.php
+++ admin/dashboard.php
@@ -12,9 +12,16 @@
     <?php require('inc/links.php'); ?>
 </head>
 <body class="bg-light">
-    <div class="container-fluid bg-primary text-light p-3 d-flex align-item-center justify-content-between">
-        <h3 class="mb-0">BẢNG ĐIỀU KHIỂN</h3>
-        <a href="logout.php" class="btn btn-light btn-sm">Đăng xuất</a>
+
+    <?php require('inc/header.php'); ?>
+
+
+    <div class="container-fluid" id="main-content">
+        <div class="row">
+            <div class="col-lg-10 ms-auto p-4 overflow-hidden">
+
+            </div>
+        </div>
     </div>
     
     <?php require('inc/scripts.php'); ?>
