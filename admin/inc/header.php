f708803753bb858c3dc17eb49d4aac36858517c3
messiuuuuuu <145339425+messiuuuuuu@users.noreply.github.com>
2023-11-24 23:18:36 +0700

    Add files via upload

---
+++ admin/inc/header.php
@@ -0,0 +1,31 @@
+<div class="container-fluid bg-primary text-light p-3 d-flex align-item-center justify-content-between sticky-top">
+    <h3 class="mb-0">BẢNG ĐIỀU KHIỂN</h3>
+    <a href="logout.php" class="btn btn-light btn-sm">Đăng xuất</a>
+</div>
+
+<div class="col-lg-2 bg-dark border-top border-3 border-secondary">
+<nav class="navbar navbar-expand-lg navbar-dark" id="dashboard-menu">
+        <div class="container-fluid flex-lg-column align-items-stretch">
+        <h4 class="mt-2 text-light">BẢNG ĐIỀU KHIỂN</h4>
+        <button class="navbar-toggler shadow-none" type="button" data-bs-toggle="collapse" data-bs-target="#adminDropdown" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse flex-column align-items-stretch mt-2" id="adminDropdown">
+            <ul class="nav nav-pills flex-column">
+                <li class="nav-item">
+                    <a class="nav-link text-white" href="#dashboard.php">Trang chủ</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link text-white" href="#">Phòng</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link text-white" href="#">Người dùng</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link text-white" href="#setting.php">Cài đặt</a>
+                </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+</div>
